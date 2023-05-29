ffe67c111a66b7a0ca9aecc9cc2e8832d86e9318
Карлос Туеста Льаха <71520076+pascaltll@users.noreply.github.com>
2023-05-29 03:12:26 +0300

    Update network .py

--- assignment02_three_headed_network/network .py
+++ assignment02_three_headed_network/network .py
@@ -1,62 +1,45 @@
-import numpy as np
-import pandas as pd
-
-import torch
-from torch import nn
-import torch.nn.functional as F
-
-import tqdm
-
-
-class ThreeInputsNet(nn.Module):
-    def __init__(self, n_tokens, n_cat_features, concat_number_of_features, hid_size=64):
-        super(ThreeInputsNet, self).__init__()
-        self.title_emb = nn.Embedding(n_tokens, embedding_dim=hid_size)
-        self.title_conv = nn.Conv1d(in_channels=hid_size, out_channels=hid_size, kernel_size=3)
-
-        self.title_pool = nn.AdaptiveAvgPool1d(output_size=1)
-
+            nn.AdaptiveAvgPool1d(output_size=1),
+            )
+        
         self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
-        self.full_conv1 = nn.Conv1d(in_channels=hid_size, out_channels=hid_size, kernel_size=3)
-        self.full_conv2 = nn.Conv1d(in_channels=hid_size, out_channels=hid_size, kernel_size=5, padding=1)
-        self.full_conv3 = nn.Conv1d(in_channels=hid_size, out_channels=hid_size, kernel_size=7, padding=2)
-        self.full_pool = nn.AdaptiveAvgPool1d(output_size=1)
-
-        self.category_out = nn.Linear(in_features=n_cat_features, out_features=2 * hid_size)
-
-        self.relu = nn.ReLU()
-
+        # <YOUR CODE HERE>
+        self.full_seq = nn.Sequential(
+            nn.Conv1d(in_channels=hid_size, out_channels=hid_size*2, kernel_size=2), 
+            nn.ReLU(), 
+            nn.BatchNorm1d(2*hid_size),
+        	nn.Conv1d(in_channels=hid_size*2, out_channels=hid_size*2, kernel_size=2),
+        	nn.ReLU(),
+        	nn.AdaptiveAvgPool1d(output_size=1),
+        	)
+        
+        self.category_out = nn.Linear(in_features=n_cat_features, out_features=hid_size)
+        
+        self.bn = nn.BatchNorm1d(concat_number_of_features)
         # Example for the final layers (after the concatenation)
-        self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size * 2)
-        self.final_dense = nn.Linear(in_features=hid_size * 2, out_features=1)
+        self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2)
+        self.final_dense = nn.Linear(in_features=hid_size*2, out_features=1)
+
 
     def forward(self, whole_input):
         input1, input2, input3 = whole_input
         title_beg = self.title_emb(input1).permute((0, 2, 1))
-        title = self.title_conv(title_beg)
-        title = self.relu(title)
-        title = self.title_pool(title)
-
+        title = self.title_seq(title_beg)# <YOUR CODE HERE>
+        
         full_beg = self.full_emb(input2).permute((0, 2, 1))
-        full_1 = self.full_conv1(full_beg)
-        full_2 = self.full_conv2(full_beg)
-        full_3 = self.full_conv3(full_beg)
-        full = torch.cat((full_1, full_2, full_3), dim=1)
-        full = self.relu(full)
-        full = self.full_pool(full)
-
-        category = self.category_out(input3)
-
+        full = self.full_seq(full_beg)# <YOUR CODE HERE>        
+        
+        category = self.category_out(input3)# <YOUR CODE HERE>        
+        
         concatenated = torch.cat(
             [
-                title.view(title.size(0), -1),
-                full.view(full.size(0), -1),
-                category.reshape(category.size(0), -1)
+            title.view(title.size(0), -1),
+            full.view(full.size(0), -1),
+            category.view(category.size(0), -1)
             ],
             dim=1)
-
-        out = self.inter_dense(concatenated)
-        out = self.relu(out)
+        
+     
+        out = self.inter_dense(self.bn(concatenated))
         out = self.final_dense(out)
-
+        
         return out
